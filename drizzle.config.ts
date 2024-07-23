8d23864a24973116e07c6aeaeeb4bc635d9655d0
kelechi <igwesiemmanuel4@gmail.com>
2024-07-23 09:35:25 +0100

    final fix on deployment

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,22 +3,31 @@ import { defineConfig } from "drizzle-kit";
 
 dotenv.config();
 
-function getEnvVar(name: string, defaultValue?: string): string {
-  const value = process.env[name];
-  if (!value && !defaultValue) {
-    throw new Error(`Environment variable ${name} is not defined`);
-  }
-  return value || defaultValue!;
-}
+// function getEnvVar(name: string, defaultValue?: string): string {
+//   const value = process.env[name];
+//   if (!value && !defaultValue) {
+//     throw new Error(`Environment variable ${name} is not defined`);
+//   }
+//   return value || defaultValue!;
+// }
+
+// export default defineConfig({
+//   schema: "./src/db/schema.ts",
+//   out: "./drizzle",
+//   dialect: "postgresql",
+//   dbCredentials: {
+//     host: getEnvVar("PG_HOST"),
+//     user: getEnvVar("PG_USER"),
+//     password: getEnvVar("PG_PASSWORD"),
+//     database: getEnvVar("PG_DATABASE"),
+//   },
+// });
 
 export default defineConfig({
+  dialect: "postgresql",
   schema: "./src/db/schema.ts",
   out: "./drizzle",
-  dialect: "postgresql",
   dbCredentials: {
-    host: getEnvVar("PG_HOST"),
-    user: getEnvVar("PG_USER"),
-    password: getEnvVar("PG_PASSWORD"),
-    database: getEnvVar("PG_DATABASE"),
+    url: process.env.PG_URL as string,
   },
 });
